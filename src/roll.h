7b5e0214ecb174d227b98a1f1374a16ed7e0c407
anika342 <anikahuqsiraze.shailee@g.austincc.edu>
2024-12-14 00:13:08 +0000

    Question 2

--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,22 @@
 //
+#ifndef ROLL_H
+#define ROLL_H
+
+#include "die.h"
+
+class Roll {
+public:
+    //Roll() : Roll() {}    //constructor
+    Roll(Die& die1, Die& die2);
+
+    void roll_dice();
+    int roll_value() const;
+
+private:
+    Die& die1;
+    Die& die2;
+    int rolled_value;
+};
+
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -15,5 +16,21 @@ TEST_CASE("Verify die roll return a value from 1 to 6") {
 		//int roll_value = die.roll();
 		REQUIRE (die.roll() == (1||2||3||4||5||6));
 		//REQUIRE ((roll_value >1) && (roll_value<7));
+	}
+}
+
+TEST_CASE ("Verify die rolls return a value from 2 to 12") {
+	Die die1;
+	Die die2;
+	Roll roll(die1, die2);
+
+	for (int i=0; i<10; ++i)
+	{
+		roll.roll_dice();
+		//int roll_value = roll.roll_value();
+
+		REQUIRE(roll.roll_value() == (2||3||4||5||6||7||8||9||10||11||12));
+		//REQUIRE(roll.roll_value() == (1));
+
 	}
 }
